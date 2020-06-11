ae47fbd8a62328561e92f77e75a1c5f4c70d77de
Halit EMEN <halitemen23@gmail.com>
2020-06-11 16:30:25 +0300

    initial

---
+++ SignalRChat/MessageHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat
+{
+    public class MessageHub : Hub
+    {
+        public Task SendMessageToAll(string message)
+        {
+            return Clients.All.SendAsync("ReceivedMessage", message);
+        }
+    }
+}
---
+++ SignalRChat/wwwroot/lib/message.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+var connection = new signalR.HubConnectionBuilder().withUrl("/messages").build();
+
+connection.on("ReceivedMessage", function (message) {
+    var msg = message;
+    var div = document.createElement("div");
+    div.innerHTML = msg + "<hr/>";
+    document.getElementById("messages").appendChild(div);
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var message = document.getElementById("message").value;
+    connection.invoke("SendMessageToAll", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
